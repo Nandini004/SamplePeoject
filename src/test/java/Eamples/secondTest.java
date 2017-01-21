af7116b6973ba751d9d1493482766871a89881a9
nvupkoor <nvupkoor@cisco.com>
2017-01-21 07:45:05 +0530

    initial commit

---
+++ src/test/java/Eamples/secondTest.java
@@ -0,0 +1,19 @@
+package Eamples;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class secondTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
---
+++ src/test/java/Examples/NewTest.java
@@ -0,0 +1,30 @@
+package Examples;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	private WebDriver driver;
+  @Test
+  public void f() {
+	  driver.get("http://www.guru99.com/selenium-tutorial.html");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Free Selenium Tutorials")); 
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new FirefoxDriver();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
